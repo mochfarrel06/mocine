83aba87f91f315c8635cb10a86d7a6c896b5983d
MochFarrel06 <mochfarrel2606@gmail.com>
2024-04-03 03:12:17 +0700

    feat: Setup Spatie

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,15 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('admin', function () {
+    return "Hello admin";
+})->middleware('role:admin');
+
+Route::get('user', function () {
+    return "Hello user";
+})->middleware('role:user');
+
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -35,4 +44,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
